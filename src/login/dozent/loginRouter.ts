c05b5179bd72bef8472be716471530a838d365bc
hunte3er <72540426+hunte3er@users.noreply.github.com>
2023-10-26 18:59:15 +0200

    .

---
+++ src/login/dozent/loginRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import {authenticateUser} from "../../common/middleware/authenticate";
+
+const loginRouter = Router();
+
+// Define the '/login/dozent' route
+loginRouter.post('/dozent', authenticateUser, (req, res) => {
+    // If the code reaches this point, it means the user is authenticated
+    // You can include additional logic or send a response here
+    res.json({ message: 'Authentication successful' });
+});
+
+export { loginRouter };
